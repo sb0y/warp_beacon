9ca61061ee510d4874bc33f3ccd3f9ebb33f8fcc
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-01 18:54:36 +0300

    fixed multithreading

---
+++ warp_beacon/uploader/__init__.py
@@ -0,0 +1,47 @@
+import threading
+import logging
+
+from typing import Optional, Callable
+
+class AsyncUploader(object):
+	threads = []
+	allow_loop = True
+	job_queue = None
+	callback = None
+
+	def __init__(self, pool_size: int=3) -> None:
+		self.job_queue = threading.Queue()
+		for _ in range(pool_size):
+			thread = threading.Thread(target=self.do_work)
+			self.threads.append(thread)
+			thread.start()
+	
+	def __del__(self) -> None:
+		pass
+
+	def set_callback(self, callback: Callable) -> None:
+		self.callback = callback
+
+	def stop_all(self) -> None:
+		self.allow_loop = False
+		for i in self.threads:
+			i.join()
+
+	def queue_task(self, local_path: str) -> None:
+		self.job_queue.put_nowait({"path": local_path})
+
+	def do_work(self) -> None:
+		while self.allow_loop:
+			try:
+				try:
+					item = self.job_queue.get()
+					try:
+						local_path = item["path"]
+						item["callback"](local_path)
+					except Exception as e:
+						logging.exception(e)
+				except threading.queue.Empty:
+					pass
+			except Exception as e:
+				logging.error("Exception occurred inside upload worker!")
+				logging.exception(e)
