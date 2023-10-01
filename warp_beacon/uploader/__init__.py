68ae12a8b28cf6f8cbf75fa0d6002b44be02a5c5
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-01 20:24:06 +0300

    fix coro

--- warp_beacon/uploader/__init__.py
+++ warp_beacon/uploader/__init__.py
@@ -15,7 +15,7 @@ class AsyncUploader(object):
 	def __init__(self, pool_size: int=3) -> None:
 		self.job_queue = multiprocessing.Queue()
 		for _ in range(pool_size):
-			thread = threading.Thread(target=lambda: asyncio.run(self.do_work()))
+			thread = threading.Thread(target=asyncio.run, args=(self.do_work(),))
 			self.threads.append(thread)
 			thread.start()
 	
