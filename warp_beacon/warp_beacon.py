3a708be515c9d318104b5ff284c4a1d2026b59b1
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-05 01:57:58 +0300

    test

--- warp_beacon/warp_beacon.py
+++ warp_beacon/warp_beacon.py
@@ -167,11 +167,13 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 	if chat.type not in (Chat.GROUP, Chat.SUPERGROUP):
 		await update.message.reply_text(reply_text, reply_to_message_id=effective_message_id)
 
-async def main() -> None:
+def main() -> None:
 	"""Start the bot."""
 	loop = asyncio.get_event_loop()
 	# Create the Application and pass it your bot's token.
 	application = Application.builder().token(os.environ.get("TG_TOKEN", default=None)).concurrent_updates(True).build()
+
+	loop = asyncio.get_event_loop()
 
 	uploader = AsyncUploader(
 		pool_size=int(os.environ.get("UPLOAD_POOL_SIZE", default=scrapler.CONST_CPU_COUNT))
@@ -188,13 +190,9 @@ async def main() -> None:
 	# on non command i.e message - echo the message on Telegram
 	application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handler))
 	# Run the bot until the user presses Ctrl-C
-	#application.run_polling(allowed_updates=Update.ALL_TYPES, stop_signals=[signal.SIGTERM, signal.SIGINT, signal.SIGQUIT])
-	async with application:
-		await application.start()
-		loop.run_forever()
-		await application.stop()
+	application.run_polling(allowed_updates=Update.ALL_TYPES, stop_signals=[signal.SIGTERM, signal.SIGINT, signal.SIGQUIT])
 	downloader.stop_all()
 	uploader.stop_all()
 
 if __name__ == "__main__":
-	asyncio.run(main())
+	main()
