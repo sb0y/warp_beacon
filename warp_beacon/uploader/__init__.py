416d1ed0b1db8fc2fa6f99f35600ec743d6e90d2
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-05 03:09:53 +0300

    test

--- warp_beacon/uploader/__init__.py
+++ warp_beacon/uploader/__init__.py
@@ -16,8 +16,9 @@ class AsyncUploader(object):
 	def __init__(self, pool_size: int=multiprocessing.cpu_count()) -> None:
 		self.job_queue = multiprocessing.Queue()
 		#do_work = lambda: asyncio.run(self.do_work())
+		loop = asyncio.get_event_loop()
 		for _ in range(pool_size):
-			thread = threading.Thread(target=asyncio.run_coroutine_threadsafe(self.do_work()))
+			thread = threading.Thread(target=asyncio.run_coroutine_threadsafe(self.do_work(), loop))
 			self.threads.append(thread)
 			thread.start()
 	
