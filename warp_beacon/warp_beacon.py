2f9d1d84b3a0bd694b96d3c875f1efebdcbc5b5f
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-05 03:58:42 +0300

    added async

--- warp_beacon/warp_beacon.py
+++ warp_beacon/warp_beacon.py
@@ -151,7 +151,7 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 				logging.info("URL '%s' is found in DB. Sending with tg_file_id = '%s'", url, tg_file_id)
 				await send_without_upload(update, context, tg_file_id, effective_message_id)
 			else:
-				def send_video_wrapper(local_media_path: str, uniq_id: str, in_process: bool=False) -> None:
+				async def send_video_wrapper(local_media_path: str, uniq_id: str, in_process: bool=False) -> None:
 					return send_video(update, context, local_media_path, url, uniq_id, in_process)
 	
 				uploader.add_callback(effective_message_id, send_video_wrapper)
