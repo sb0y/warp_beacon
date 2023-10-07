07e631df91f0afcf0edceb5ebfe5a3dac0770a71
Andrey Bagrintsev <a.bagrintsev@petrosoftinc.com>
2023-10-07 19:36:47 +0300

    test

--- warp_beacon/warp_beacon.py
+++ warp_beacon/warp_beacon.py
@@ -144,7 +144,7 @@ async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 				async def send_video_wrapper(local_media_path: str, uniq_id: str, tg_file_id: str=None) -> None:
 					return await send_video(update, context, local_media_path, url, uniq_id, tg_file_id)
 					
-				uploader.add_callback(effective_message_id, send_video_wrapper())
+				uploader.add_callback(effective_message_id, send_video_wrapper)
 
 				logging.info("Downloading URL '%s' from instagram ...", url)
 				try:
