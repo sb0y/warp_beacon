21c9588f5a89451b4cd58494504a7f1c9b8e971b
sb0y <andrey@bagrintsev.me>
2024-07-21 13:38:39 +0000

    fixed video thumbs generation bug; added new dev dependencies; removed deb tmp files;

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 setup.py clean
-rm -Rf deb_dist/ dist/ include/ build/ lib/ debian/.debhelper/ warp_beacon-*.tar.gz warp_beacon*.egg-info
+rm -Rf deb_dist/ dist/ include/ build/ lib/ debian/.debhelper/ debian/warp-beacon warp_beacon-*.tar.gz warp_beacon*.egg-info
 
 exit 0
