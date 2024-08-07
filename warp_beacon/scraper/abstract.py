c571437298f008c6bb644b509c8d4f69a13c8a1b
sb0y <andrey@bagrintsev.me>
2024-08-07 16:32:35 +0000

    Added heic images support

--- warp_beacon/scraper/abstract.py
+++ warp_beacon/scraper/abstract.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from typing import Callable, Union
 
 from PIL import Image
+from pillow_heif import register_heif_opener
 
 import logging
 
@@ -41,4 +42,21 @@ class ScraperAbstract(ABC):
 
 		return ''
 
+	@staticmethod
+	def convert_heic_to_png(src_file: str) -> str:
+		try:
+			if os.path.exists(src_file):
+				register_heif_opener()
+				path_info = pathlib.Path(src_file)
+				old_filename = path_info.stem
+				new_filename = "%s_converted.%s" % (old_filename, "png")
+				new_filepath = "%s/%s" % (os.path.dirname(src_file), new_filename)
+				with Image.open(src_file).convert('RGB') as img:
+					img.save(new_filepath, 'png')
+				os.unlink(src_file)
+				return new_filepath
+		except Exception as e:
+			logging.error("Failed to convert webp file to png!")
+			logging.exception(e)
 
+		return ''
