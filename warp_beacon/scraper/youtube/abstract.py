41321b673559f5c6dac08e384c7fdc60db374380
sb0y <andrey@bagrintsev.me>
2024-11-03 03:00:13 +0000

    added threshold for yt thumbs crop

--- warp_beacon/scraper/youtube/abstract.py
+++ warp_beacon/scraper/youtube/abstract.py
@@ -139,9 +139,9 @@ class YoutubeAbstract(ScraperAbstract):
 		gcd = math.gcd(size[0], size[1])
 		return size[0] // gcd, size[1] // gcd
 	
-	def crop_black_edges_pil(self, img: Image) -> Image:
-		y_nonzero, x_nonzero, _ = np.nonzero(img)
-		return img.crop((np.min(x_nonzero), np.min(y_nonzero), np.max(x_nonzero), np.max(y_nonzero)))
+	def crop_black_edges_pil(self, img: Image, threshold: int = 35) -> Image:
+		y_nonzero, x_nonzero, _ = np.nonzero(np.array(img) > threshold)
+		return img.crop((np.min(x_nonzero), np.min(y_nonzero)+15, np.max(x_nonzero), np.max(y_nonzero)-5))
 
 	def download_thumbnail(self, video_id: str, timeout: int) -> Optional[io.BytesIO]:
 		for i in (#"https://img.youtube.com/vi/{VIDEO_ID}/maxresdefault.jpg",
