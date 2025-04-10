fa587d93836849793c9f7fa8d4c9b4795c0f206a
sb0y <andrey@bagrintsev.me>
2025-04-10 09:43:31 +0000

    switch to yt_dlp, if pytubefix.exceptions raised

--- warp_beacon/scraper/youtube/abstract.py
+++ warp_beacon/scraper/youtube/abstract.py
@@ -180,7 +180,7 @@ class YoutubeAbstract(ScraperAbstract):
 				# do noting, not interested
 				pass
 			#except http.client.IncompleteRead as e:
-			except KeyError:
+			except (KeyError, pytubefix.exceptions.RegexMatchError):
 				raise Unavailable("Library failed")
 			except urllib3.exceptions.ProxyError as e:
 				logging.warning("Proxy error!")
